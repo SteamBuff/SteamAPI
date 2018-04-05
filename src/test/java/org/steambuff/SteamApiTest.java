6ac5b324a00628f72fc595b9af5d5c1b09dacd52
Alexander <gasfull98@gmail.com>
2018-04-05 10:40:22 +0300

    fix Broken...

---
+++ src/test/java/org/steambuff/SteamApiTest.java
@@ -0,0 +1,19 @@
+package org.steambuff;
+
+import org.junit.jupiter.api.Test;
+import org.steambuff.exception.SteamApiException;
+import org.steambuff.method.ListSteamId;
+import org.steambuff.method.SteamId;
+import org.steambuff.method.steamuser.entity.UserStats;
+
+
+public class SteamApiTest {
+
+    public static String GOOD_KEY = "good";
+    public static String BAD_KEY = "bad";
+
+    @Test
+    void test() throws SteamApiException {
+    }
+
+}
